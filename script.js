75284ffcdbc53b64389e6ac7a9ed8ac74580d38f
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2023-11-08 07:41:53 -0600

    Added functions for player and computer selections

--- script.js
+++ script.js
@@ -1,8 +1,52 @@
 // js for rock paper scissors
 function getComputerChoice () {
-    const options = ["Rock", "Paper", "Scissors"];
+    const options = ["rock", "paper", "scissors"];
     const choice = options[Math.floor(Math.random()*options.length)];
     return choice;
 }
 
-console.log(getComputerChoice());
+function playRound (playerSelection, computerSelection) {
+    // Make the user's selection case insensitive:
+    playerSelection = playerSelection.toLowerCase();
+
+    // if statements for player choosing rock
+    if(playerSelection.equals("rock")){
+        if(computerSelection.equals("rock")){
+            return ("Draw! Try again.");
+        }
+        if(computerSelection.equals("paper")){
+            return ("You lose! Paper wins over rock.");
+        }
+        if(computerSelection.equals("scissors")){
+            return ("You win! Rock wins over scissors.");
+        }
+    }
+
+    // if statements for player choosing paper
+    if(playerSelection.equals("paper")){
+        if(computerSelection.equals("rock")){
+            return ("You win! Paper wins over rock.");
+        }
+        if(computerSelection.equals("paper")){
+            return ("Draw! Try again.");
+        }
+        if(computerSelection.equals("scissors")){
+            return ("You lose! Scissors win over paper.");
+        }
+    }
+    
+    // if statements for player choosing scissors
+    if(playerSelection.equals("scissors")){
+        if(computerSelection.equals("rock")){
+            return ("You lose! Rock wins over scissors.");
+        }
+        if(computerSelection.equals("paper")){
+            return ("You win! Scissors win over paper.");
+        }
+        if(computerSelection.equals("scissors")){
+            return ("Draw! Try again.");
+        }
+    }
+}
+
+const computerSelection = getComputerChoice();
