b5fd3dc3d92939f68c409614c9661d23823dd6be
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2023-11-08 10:49:22 -0600

    Update script.js
    FInished working implementation of working rock paper scissors game.

--- script.js
+++ script.js
@@ -15,9 +15,11 @@ function playRound (playerSelection, computerSelection) {
             return ("Draw! Try again.");
         }
         if(computerSelection=="paper"){
+            computerScore++;
             return ("You lose! Paper wins over rock.");
         }
         if(computerSelection=="scissors"){
+            playerScore++;
             return ("You win! Rock wins over scissors.");
         }
     }
@@ -25,12 +27,14 @@ function playRound (playerSelection, computerSelection) {
     // if statements for player choosing paper
     if(playerSelection=="paper"){
         if(computerSelection=="rock"){
+            playerScore++;
             return ("You win! Paper wins over rock.");
         }
         if(computerSelection=="paper"){
             return ("Draw! Try again.");
         }
         if(computerSelection=="scissors"){
+            computerScore++;
             return ("You lose! Scissors win over paper.");
         }
     }
@@ -38,9 +42,11 @@ function playRound (playerSelection, computerSelection) {
     // if statements for player choosing scissors
     if(playerSelection=="scissors"){
         if(computerSelection=="rock"){
+            computerScore++;
             return ("You lose! Rock wins over scissors.");
         }
         if(computerSelection=="paper"){
+            playerScore++;
             return ("You win! Scissors win over paper.");
         }
         if(computerSelection=="scissors"){
@@ -49,8 +55,32 @@ function playRound (playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = "scissors";
-const computerSelection = getComputerChoice();
+function game () {
+    // Play 5 rounds
+    for (let i = 0; i<5;i++){
+        playerSelection = window.prompt("Choose rock, paper, or scissors.");
+        computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+        // Might add an alert in here to alert the user that they have
+        // either lost or won. 
+    }
 
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+    // Check to see who is the winner
+    if(computerScore>playerScore){
+        return "You Lose!";
+    }
+    else {
+        return "You win!!!";
+    }
+
+}
+
+// Define varaibles
+let computerScore = 0;
+let playerScore = 0;
+let playerSelection;
+let computerSelection;
+
+console.log(game());
+console.log(computerScore);
+console.log(playerScore);
