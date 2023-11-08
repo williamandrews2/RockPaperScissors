8cb03ac751988899175ade4d6e63d5cda240c71d
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2023-11-07 22:02:27 -0600

    Update script.js
    Added function for the computer to choose a random rock, paper, or scissor.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // js for rock paper scissors
+function getComputerChoice () {
+    const options = ["Rock", "Paper", "Scissors"];
+    const choice = options[Math.floor(Math.random()*options.length)];
+    return choice;
+}
+
+console.log(getComputerChoice());
