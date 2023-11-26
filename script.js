ff31dddad8dea298bc2bb86bfb80cd7b85bd3e42
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2023-11-25 18:54:43 -0600

    Added scoreboard

--- script.js
+++ script.js
@@ -1,8 +1,57 @@
-// js for rock paper scissors
+
+// Define varaibles
+let computerScore = 0;
+let playerScore = 0;
+let playerSelection;
+let computerSelection;
+
+const container = document.querySelector("#container");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const player = document.querySelector("#player");
+const opponent = document.querySelector("#opponent");
+
+// Create new DOM elements
+const result = document.createElement("div");
+result.classList.add("result");
+const playerScoreboard = document.createElement("h1");
+playerScoreboard.classList.add("playerScoreboard");
+const opponentScoreboard = document.createElement("h1");
+opponentScoreboard.classList.add("opponentScoreboard");
+
+// Append DOM elements
+container.appendChild(result);
+player.appendChild(playerScoreboard);
+opponent.appendChild(opponentScoreboard);
+
+// eventListeners
+rock.addEventListener("click", () => {
+    computerSelection = getComputerChoice();
+    console.log(playRound("rock", computerSelection));
+});
+
+paper.addEventListener("click", () => {
+    computerSelection = getComputerChoice();
+    console.log(playRound("paper", computerSelection));
+});
+
+scissors.addEventListener("click", () => {
+    computerSelection = getComputerChoice();
+    console.log(playRound("scissors", computerSelection));
+});
+
+// functions:
+
 function getComputerChoice () {
     const options = ["rock", "paper", "scissors"];
     const choice = options[Math.floor(Math.random()*options.length)];
     return choice;
+}
+
+function displayScore () {
+    playerScoreboard.textContent = playerScore;
+    opponentScoreboard.textContent = computerScore;
 }
 
 function playRound (playerSelection, computerSelection) {
@@ -13,14 +62,17 @@ function playRound (playerSelection, computerSelection) {
     if(playerSelection=="rock"){
         if(computerSelection=="rock"){
             result.textContent = "Draw! Try again.";
+            displayScore();
         }
         if(computerSelection=="paper"){
             computerScore++;
             result.textContent = "You lose! Paper wins over rock.";
+            displayScore();
         }
         if(computerSelection=="scissors"){
             playerScore++;
             result.textContent = "You win! Rock wins over scissors.";
+            displayScore();
         }
     }
 
@@ -29,13 +81,16 @@ function playRound (playerSelection, computerSelection) {
         if(computerSelection=="rock"){
             playerScore++;
             result.textContent = "You win! Paper wins over rock.";
+            displayScore();
         }
         if(computerSelection=="paper"){
             result.textContent =  "Draw! Try again.";
+            displayScore();
         }
         if(computerSelection=="scissors"){
             computerScore++;
             result.textContent =  "You lose! Scissors win over paper.";
+            displayScore();
         }
     }
     
@@ -44,13 +99,16 @@ function playRound (playerSelection, computerSelection) {
         if(computerSelection=="rock"){
             computerScore++;
             result.textContent = "You lose! Rock wins over scissors.";
+            displayScore();
         }
         if(computerSelection=="paper"){
             playerScore++;
             result.textContent = "You win! Scissors win over paper.";
+            displayScore();
         }
         if(computerSelection=="scissors"){
             result.textContent =  "Draw! Try again.";
+            displayScore();
         }
     }
 }
@@ -72,36 +130,6 @@ function game () {
     // }
 
 }
-
-// Define varaibles
-let computerScore = 0;
-let playerScore = 0;
-let playerSelection;
-let computerSelection;
-
-const container = document.querySelector("#container");
-const rock = document.querySelector("#rock");
-const paper = document.querySelector("#paper");
-const scissors = document.querySelector("#scissors");
-const result = document.createElement("div");
-result.classList.add("result");
-
-rock.addEventListener("click", () => {
-    computerSelection = getComputerChoice();
-    console.log(playRound("rock", computerSelection));
-});
-
-paper.addEventListener("click", () => {
-    computerSelection = getComputerChoice();
-    console.log(playRound("paper", computerSelection));
-});
-
-scissors.addEventListener("click", () => {
-    computerSelection = getComputerChoice();
-    console.log(playRound("scissors", computerSelection));
-});
-
-container.appendChild(result);
 
 console.log(game());
 console.log(computerScore);
